96bc45f9a6c8b466419ac72b0916d72821082169
Chris Lloyd <chris.lloyd.hi+git@gmail.com>
2021-10-18 23:05:32 +1300

    BOARD: Add AWS IOT Code

--- ESP8266-Board/Teams-Lighting-Controller/Teams-Lighting-Controller.ino
+++ ESP8266-Board/Teams-Lighting-Controller/Teams-Lighting-Controller.ino
@@ -5,11 +5,22 @@
 
 #include "config.h"
 #include "WifiHelpers.h"
-#include <ESP8266WebServer.h>
+#include <PubSubClient.h>
 #include <ESP8266mDNS.h>
 
-ESP8266WebServer server(80);
 int ledValues[3] = {0, 0, 0};
+unsigned long lastPublish;
+int msgCount;
+
+
+// AWS IOT Config
+BearSSL::X509List client_crt(CONFIG_PEM_CRT);
+BearSSL::PrivateKey client_key(CONFIG_PEM_KEY);
+BearSSL::X509List rootCert(CONFIG_CERT_CA);
+
+WiFiClientSecure wiFiClient;
+void msgReceived(char* topic, byte* payload, unsigned int len);
+PubSubClient pubSubClient(CONFIG_AWS_ENDPOINT, 8883, msgReceived, wiFiClient); 
 
 void setup() {
   // Configure LED pins for output
@@ -18,51 +29,76 @@ void setup() {
   pinMode(GREEN_LED, OUTPUT);
 
   Serial.begin(9600);
-  
+
+  // Connect to WiFi
   pulseLeds(false, false, true);
   connectWiFi(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
 
+  // get current time, otherwise certificates are flagged as expired
+  setCurrentTime();
+
+  // Connect to AWS IOT
+  wiFiClient.setClientRSACert(&client_crt, &client_key);
+  wiFiClient.setTrustAnchors(&rootCert);
+
   // API Server
-  restServerRouting();
-  Serial.printf("Starting Webserver...");
-  server.begin();
   pulseLeds(false, true, true);
 }
 
 void loop() {
-//  pulseLeds(true, false, false);
-//  pulseLeds(false, true, false);
-  server.handleClient();
-
+  pubSubCheckConnect();
 }
 
 
 /*
- * WEBSERVER
+ * IOT PUB/SUB
  */
-void setLedsWeb() {
-  int red = server.arg("red").toInt();
-  int green = server.arg("green").toInt();
-  int blue = server.arg("blue").toInt();
-  setLeds(red, green, blue);
-
-  char responseJson[200];
-  sprintf(responseJson, "{\"red\": %d, \"green\": %d, \"blue\": %d}", red, green, blue);
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.send(200, "application/json", responseJson);
+void msgReceived(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received on "); Serial.print(topic); Serial.print(": ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
- 
-void restServerRouting() {
-  server.on("/", HTTP_GET, []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-      server.send(200, F("text/html"),
-          F("Teams On-Air Light API"));
-  });
-  server.on(F("/leds"), HTTP_POST, setLedsWeb);
-  server.on(F("/alive"), HTTP_GET, []() {
-    server.sendHeader("Access-Control-Allow-Origin", "*");
-    server.send(200, F("application/json"), F("{\"status\":\"online\"}"));
-  });
+
+void pubSubCheckConnect() {
+  if ( ! pubSubClient.connected()) {
+    Serial.print("PubSubClient connecting to: "); Serial.print(CONFIG_AWS_ENDPOINT);
+    while ( ! pubSubClient.connected()) {
+      Serial.print(".");
+      pubSubClient.connect("Julie");
+    }
+    Serial.println(" connected");
+
+    // Subscribe to topics
+    pubSubClient.subscribe("/shadow/name/main/get/accepted");
+    Serial.println("Subscribed to topic $aws/things/Julie/shadow/name/main/get/accepted");
+    pubSubClient.subscribe("$aws/things/Julie/shadow/name/main/update/accepted");
+    Serial.println("Subscribed to topic $aws/things/Julie/shadow/name/main/update/accepted");
+
+    pulseLeds(true, true, true);
+
+    // Get shadow state from AWS IOT
+    pubSubClient.publish("$aws/things/Julie/shadow/name/main/get", "");
+    Serial.print("Requested Shadow State from AWS...");    
+  }
+  pubSubClient.loop();
+}
+
+void setCurrentTime() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: "); Serial.print(asctime(&timeinfo));
 }
 
 
@@ -105,7 +141,7 @@ void connectWiFi(char *ssid, char *password)
         // start from mac[0] to mac[5], but some other kinds of board run in the oppsite direction.
         uint8_t mac[6];
         WiFi.macAddress(mac);
-        Serial.printf("You device (%02x:%02x:%02x:%02x:%02x:%02x) failed to connect to %s (%s). Waiting ~5 seconds to retry.\r\n",
+        Serial.printf("Device (%02x:%02x:%02x:%02x:%02x:%02x) failed to connect to %s (%s). Waiting ~5 seconds to retry.\r\n",
                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ssid, wl_status_to_string(WiFi.status()));
         WiFi.begin(ssid, password);
 
@@ -114,5 +150,5 @@ void connectWiFi(char *ssid, char *password)
           pulseLeds(false, false, true);
         }
     }
-    Serial.printf("Connected to wifi %s.\r\n", ssid);
+    Serial.printf("Connected to wifi %s (%s).\r\n", ssid, WiFi.localIP().toString().c_str());
 }
