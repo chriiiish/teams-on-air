fe52001c472008b337c74df4c80a84e62915fa02
Chris Lloyd <chris.lloyd.hi+git@gmail.com>
2021-10-19 10:26:11 +1300

    WEB: Hack with sound to keep tab alive (ITS ALIIIIIIIIIIIVE)

--- web/src/App.js
+++ web/src/App.js
@@ -10,6 +10,13 @@ import MicrosoftConnectTile from './components/MicrosoftConnectTile';
 const pca = new PublicClientApplication(msalConfig);
 
 class App extends React.Component {
+
+  componentDidMount(){
+    // This little hack keeps the tab alive
+    const audioEl = document.getElementById("audio");
+    audioEl.play();
+  }
+
   render() {
     return (
       <div>
@@ -18,6 +25,9 @@ class App extends React.Component {
             <MicrosoftConnectTile />
           </MsalProvider>
         </div>
+        <audio id="audio" loop={true}>
+          <source src="./1-minute-of-silence.mp3"></source>
+        </audio>
         <footer>Made by Chris</footer>
       </div>
     );
